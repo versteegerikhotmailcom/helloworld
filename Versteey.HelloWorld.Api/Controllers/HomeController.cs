cc40b284d1dff7fe1f57690c5fba5ec6f7722e5a
Erik Versteeg <versteegerik@hotmail.com>
2025-11-29 10:01:29 +0100

    First commit

---
+++ Versteey.HelloWorld.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Versteey.HelloWorld.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hellow world");
+        }
+    }
+}
